14e73131f4fde759988fde1bb981d301b6992fbb
25kq1a4752-ops <25kq1a4752@pace.ac.in>
2025-11-01 02:39:15 -0700

    Add files via upload

---
+++ PAVAN.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int a,b,sum,sub,division,multiplication;
+clrscr();
+printf("enter a,b values:");
+scanf("%d %d",&a,&b);
+sum=a+b;
+sub=a-b;
+division=a/b;
+multiplication=a*b;
+printf("%d\n",sum);
+printf("%d\n",sub);
+printf("%d\n",division);
+printf("%d\n",multiplication);
+getch();
+}
+
+
+
